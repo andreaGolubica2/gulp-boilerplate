6cd37cec8a470187aa73d9b8b5010dd243e7292f
programer10 <programer10@ucione.local>
2019-06-10 19:51:23 +0200

    Setup gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const
+    //dev mode
+    devBuild = true, 
+    // modules
+    gulp = require('gulp'),
+    newer = require('gulp-newer'),
+    noop = require('gulp-noop'),
+    htmlclean = require('gulp-htmlclean'),
+    sync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    terser = require('gulp-terser'),
+
+    // paths
+    src = '/src',
+    build = '/build';
+
+//HTML processing
+function html(){
+    return  gulp.src(src + '/html/**/*')
+    .pipe(newer(build))
+    .pipe(devBuild ? noop() : htmlclean())
+    .pipe(gulp.dest(build))
+    .pipe(sync.string());
+}
+
+ //CSS processing
+function css(){
+    const out = build + '/assets/css'
+
+    return gulp.src(src + '/scss/main.scss')
+        .pipe(sass({
+            errorLogToConsole: devBuild,
+            outputStyle: devBuild ? 'expanded' : 'compressed'
+
+        })).on('error', sass.logError)
+        .pipe(gulp.dest(out))
+        .pipe(sync.stream());
+    }
+
+//Javascript processing
+function js(){
+    const out = build + '/assets/js'
+
+    return gulp.src([
+            src + '/js/**/*'
+    ])
+    .pipe(newer(out))
+    .pipe(concat('bundle.min.js'))
+    .pipe(devBuild ? noop() : terser())
+    .pipe(gulp.dest(out))
+    .pipe(sync.stream());
+}
+
+//Watch for file changes
+function watch(done){
+    sync.init({
+        server: {
+            baseDir: '.' + build
+        }
+    });
+
+    //HTML changes
+    gulp.watch(src + '/html/**/*', html); 
+     //CSS changes
+     gulp.watch(src + '/scss/**/*', css); 
+     //JS changes
+    gulp.watch(src + '/js/**/*', js); 
+    //Reload browser
+    sync.reload();
+
+    done();
+}
+
+//Create gulp single tasks
+
+  // create gulp single tasks
+  exports.html = html;
+  exports.css = css;
+  exports.js = js;
+  exports.watch = watch;
+
+  //run all tasks
+  exports.build = gulp.parallel(exports.html, exports.css, exports.js);                
+
+  //default tasks 
+  exports.default = gulp.series(exports.build, exports.watch);
